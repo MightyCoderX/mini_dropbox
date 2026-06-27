74f632d5498c8cf2ded46df98a2c5d16e8bae93a
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2026-06-27 15:32:17 +0200

    feat: add error checking and some debug printing

--- src/chunk.c
+++ src/chunk.c
@@ -39,20 +39,25 @@ ssize_t chunk_send(Chunk* self, int sockfd)
     Message msg;
 
     msg_init(&msg, MSGTYPE_SEND_CHUNK, self->data, self->hdr.length);
-
-    msg_send(&msg, sockfd, (byte*)&self->hdr, sizeof(self->hdr));
+    int ret = msg_send(&msg, sockfd, (byte*)&self->hdr, sizeof(self->hdr));
+    if (ret < 0) return ret;
 
     return self->hdr.length;
 }
 
 ssize_t chunk_recv(int sockfd, Chunk* self)
 {
-    Message msg;
-    msg_recv_header(sockfd, &msg);
+    Message msg = { 0 };
+    int ret = msg_recv_header(sockfd, &msg);
+    if (ret < 0)
+    {
+        perror("msg_recv_header");
+        return ret;
+    }
 
     if (msg.hdr.length < sizeof(ChunkHdr))
     {
-        fprintf(stderr, "chunk received was smaller than ChunkHdr\n");
+        fprintf(stderr, "chunk received was smaller than ChunkHdr %zu\n", msg.hdr.length);
         return -1;
     }
 
